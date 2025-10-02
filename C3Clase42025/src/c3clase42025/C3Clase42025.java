c636fa614c49cd4c11d34efb859898369e86d801
dnunezFidelitas <133621200+dnunezFidelitas@users.noreply.github.com>
2025-10-01 19:57:08 -0600

    Add files via upload

---
+++ C3Clase42025/src/c3clase42025/C3Clase42025.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package c3clase42025;
+
+/**
+ *
+ * @author viti
+ */
+public class C3Clase42025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ C3Clase42025/src/c3clase42025/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c3clase42025;
+
+/**
+ *
+ * @author viti
+ */
+public class Cliente {
+    //Atributos
+    public int cedula;
+    public String nombre;
+    public int anioNacimiento;
+    
+    //metodos
+    public void pagarFactura(){
+    
+    }
+    
+}
